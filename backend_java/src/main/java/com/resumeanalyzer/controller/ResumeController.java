bdc86792dac2e9830f356fa3e8f0aa2a166dcb6f
tanvirkashyap <tanvi.gm@gmail.com>
2026-05-17 13:58:30 +0530

    file completed

--- backend_java/src/main/java/com/resumeanalyzer/controller/ResumeController.java
+++ backend_java/src/main/java/com/resumeanalyzer/controller/ResumeController.java
@@ -1,6 +1,7 @@
 package com.resumeanalyzer.controller;
 
 import com.resumeanalyzer.dto.ResumeResponseDTO;
+import com.resumeanalyzer.exception.ResumeProcessingException;
 import com.resumeanalyzer.service.ResumeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,4 +15,18 @@ public class ResumeController {
     private final ResumeService resumeService;
 
     public ResumeController(ResumeService resumeService) {
-        thi
+        this.resumeService = resumeService;
+    }
+
+    @PostMapping("/analyze")
+    public ResponseEntity<ResumeResponseDTO> analyzeResume(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam(required = false) String jobDescription
+    ) {
+        ResumeResponseDTO response = resumeService.analyzeResume(
+                file,
+                jobDescription
+        );
+        return ResponseEntity.ok(response);
+    }
+}
